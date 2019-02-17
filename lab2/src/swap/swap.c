05efd483bc5a4831c9d5720fb1a4e5ae3fb44738
Vladooha <>
2019-02-17 16:33:40 +0000

    ex1

--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -1,6 +1,14 @@
 #include "swap.h"
+#include "stdlib.h"
 
 void Swap(char *left, char *right)
 {
 	// ваш код здесь
+	char *temp = malloc(sizeof(char));
+	
+	*temp = *left;
+    *left = *right;
+    *right = *temp;
+
+    free(temp);
 }
